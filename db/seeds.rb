9491c1cd3ba1f0c959b0c28f34c08803d56e885c
Sven den Boer <denboer.sven@gmail.com>
2018-08-06 18:59:54 +0800

    added 7 mock data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+mock = [
+    {
+        image_url: 'https://loremflickr.com/320/240',
+        description: 'lovely road', 
+        latitude: 44.07975,
+        longitude: 146.75697,
+        user_id: 1 
+    }, 
+    {
+        image_url: 'https://loremflickr.com/320/240',
+        description: 'nice mountain', 
+        latitude:  -80.72885,
+        longitude: -13.81245,
+        user_id: 2 
+    }, 
+    {
+        image_url: 'https://loremflickr.com/320/240',
+        description: 'very hot', 
+        latitude: -34.74393,
+        longitude: -52.49895,
+        user_id: 3 
+    }, 
+    {
+        image_url: 'https://loremflickr.com/320/240',
+        description: 'twisties!', 
+        latitude: 18.54510,
+        longitude: 70.15373,
+        user_id: 4 
+    }, 
+    {
+        image_url: 'https://loremflickr.com/320/240',
+        description: 'rocks', 
+        latitude: 41.54349,
+        longitude: 23.90847,
+        user_id: 5 
+    }, 
+    {
+        image_url: 'https://loremflickr.com/320/240',
+        description: 'super corners', 
+        latitude:  -21.64221,
+        longitude: -63.09435,
+        user_id: 6 
+    }, 
+    {
+        image_url: 'https://loremflickr.com/320/240',
+        description: 'coffee club', 
+        latitude: 7.94205,
+        longitude: 16.50686,
+        user_id: 7 
+    }
+]
+
+Photo.create(mock)
+
+puts "#{mock.length} stories have been added"
